d85edd01181e2f701ef2c625b159f12ea09b7c19
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-02-03 19:33:15 -0700

    Add files via upload
    wk 3 assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // -- creating magic number
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 11);
+
+        // get user number
+        // Console.Write("Guess what the magic number is (1 to 10)? ");
+
+        // display results
+        Console.WriteLine($"What is the magic number? {randomNumber} ");
+
+        int userNumber = 0;
+
+        while (userNumber != randomNumber)
+        {
+            Console.Write($"What is your guess? ");
+            string userInput = Console.ReadLine();
+            // convert from string to integer
+            userNumber = int.Parse(userInput);
+
+            if (userNumber > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } 
+
     }
 }
