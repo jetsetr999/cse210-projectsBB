691c325860caa05241e64ff1216b4bca3b4bb3d0
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-02-03 19:32:30 -0700

    Add files via upload
    wk3 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        //Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradeValue = int.Parse(userInput);
+
+        //Console.WriteLine(gradeValue);
+
+       if (gradeValue >= 90)
+       {
+        letter = "A";
+       }
+        else if (gradeValue >= 80)
+       {
+        letter = "B";
+       }
+        else if (gradeValue >= 70)
+       {
+        letter = "C";
+       }
+        else if (gradeValue >= 60)
+       {
+        letter = "D";
+       }
+       else 
+       {
+        letter = "F";
+       }
+
+       Console.WriteLine($"Your grade is {letter} ");
+
+       //if (letter == "A" || letter == "B" || letter == "C")
+       if (gradeValue >= 70)
+       {
+        Console.WriteLine(" ----- Congratulations you passed ----- ");
+       }
+       else
+       {
+        Console.WriteLine("--- Sorry you failed seek help if needed and try again ---");
+       }
+
+       
     }
 }
